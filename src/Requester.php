da23a29db62607b42555c73f8297871624e4ebda
Jean-Michel Rey <vreb87@gmail.com>
2021-11-02 18:29:11 +0100

    ok pour l'envoi

--- src/Requester.php
+++ src/Requester.php
@@ -39,7 +39,6 @@ class Requester
         $client = $this->getClient();
         $request = new \xmlrpcmsg('requester.requestTransaction', [$transactionRequest->buildRpcValues()]);
         $response = &$client->send($request);
-        $b = $response->value();
 
 
         if (!$response->faultCode()) {
