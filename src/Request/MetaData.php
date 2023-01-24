6a0990dc689a2af7c4f0da5da2ec10cbc419d066
Jean-Michel Rey <vreb87@gmail.com>
2023-01-24 14:05:08 +0100

    ajout de metadata dans universign

---
+++ src/Request/MetaData.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace HumanToComputer\Universign\Request;
+
+/**
+ * @method string getIdFileAssurea()
+ * @method self setIdFileAssurea(string $idFileAssurea)
+ */
+
+class MetaData extends Base
+{
+	protected $attributesTypes = [
+		'innerMetadata' => 'array',
+	];
+}
--- src/Request/TransactionDocument.php
+++ src/Request/TransactionDocument.php
@@ -4,36 +4,39 @@ namespace HumanToComputer\Universign\Request;
 
 /**
  * @method string getDocumentType()
- * @method setDocumentType(string $documentType)
+ * @method self setDocumentType(string $documentType)
  * @method string getContent()
- * @method setContent(string $content)
+ * @method self setContent(string $content)
  * @method string getUrl()
- * @method setUrl(string $url)
+ * @method self setUrl(string $url)
  * @method string getName()
- * @method setName(string $name)
+ * @method self setName(string $name)
  * @method string getTitle()
- * @method setTitle(string $title)
+ * @method self setTitle(string $title)
+ * @method string getMetaData()
+ * @method self setMetaData(MetaData $metaData)
  * @method SEPAData getSEPAData()
- * @method setSEPAData(SEPAData $sEPAData)
+ * @method self setSEPAData(SEPAData $sEPAData)
  * @method DocSignatureField[] getSignatureFields()
- * @method setSignatureFields(DocSignatureField[] $signatureFields)
+ * @method self setSignatureFields(DocSignatureField[] $signatureFields)
  */
 
 class TransactionDocument extends Base
 {
-    protected $attributesTypes = [
-        'documentType'    => 'string',
-        'content'         => 'base64',
-        'url'             => 'string',
-        'name'            => 'string',
-        'title'           => 'string',
-        'SEPAData' => SEPAData::class,
-        'signatureFields' => 'array',
-    ];
+	protected $attributesTypes = [
+		'documentType'    => 'string',
+		'content'         => 'base64',
+		'url'             => 'string',
+		'name'            => 'string',
+		'title'           => 'string',
+		'metaData'           => MetaData::class,
+		'SEPAData' => SEPAData::class,
+		'signatureFields' => 'array',
+	];
 
-    public function addSignatureField(DocSignatureField  $docSignatureField): self
-    {
-        $this->attributes['signatureFields'][] = $docSignatureField;
-        return $this;
-    }
+	public function addSignatureField(DocSignatureField  $docSignatureField): self
+	{
+		$this->attributes['signatureFields'][] = $docSignatureField;
+		return $this;
+	}
 }
